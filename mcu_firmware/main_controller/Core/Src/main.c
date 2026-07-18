948d6b00e37c131da3c78b6c37cab3f1d801f2dc
thevinufernando <thevinuworks1@gmail.com>
2026-07-19 02:02:22 +0530

    Configured drive motors with DRV8251A

--- mcu_firmware/main_controller/Core/Src/main.c
+++ mcu_firmware/main_controller/Core/Src/main.c
@@ -26,6 +26,7 @@
 #include "ICM42688.h"
 #include "usbd_cdc_if.h"
 #include "encoders.h"
+#include "DRV8251A.h"
 #include <stdio.h>
 
 /* LIS2MDL is unpopulated/faulty on this board revision (I2C NACK, see PB6/PB7).
@@ -60,6 +61,7 @@ SPI_HandleTypeDef hspi1;
 
 TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
 
 /* Definitions for IMU_Handler */
 osThreadId_t IMU_HandlerHandle;
@@ -72,6 +74,13 @@ const osThreadAttr_t IMU_Handler_attributes = {
 osThreadId_t Encoder_HandlerHandle;
 const osThreadAttr_t Encoder_Handler_attributes = {
   .name = "Encoder_Handler",
+  .stack_size = 256 * 4,
+  .priority = (osPriority_t) osPriorityHigh,
+};
+/* Definitions for MainMotorDriver */
+osThreadId_t MainMotorDriverHandle;
+const osThreadAttr_t MainMotorDriver_attributes = {
+  .name = "MainMotorDriver",
   .stack_size = 256 * 4,
   .priority = (osPriority_t) osPriorityHigh,
 };
@@ -96,8 +105,10 @@ static void MX_I2C1_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_TIM3_Init(void);
 void IMUTask(void *argument);
 void EncoderTask(void *argument);
+void MainMotorTask(void *argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -141,10 +152,16 @@ int main(void)
   MX_SPI1_Init();
   MX_TIM1_Init();
   MX_TIM2_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 
   HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_ALL);
   HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
+
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
 
   /* USER CODE END 2 */
 
@@ -173,6 +190,9 @@ int main(void)
 
   /* creation of Encoder_Handler */
   Encoder_HandlerHandle = osThreadNew(EncoderTask, NULL, &Encoder_Handler_attributes);
+
+  /* creation of MainMotorDriver */
+  MainMotorDriverHandle = osThreadNew(MainMotorTask, NULL, &MainMotorDriver_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -439,6 +459,67 @@ static void MX_TIM2_Init(void)
 }
 
 /**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 2399;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -490,6 +571,8 @@ static void MX_GPIO_Init(void)
 /* USER CODE END Header_IMUTask */
 void IMUTask(void *argument)
 {
+  /* init code for USB_DEVICE */
+  MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 5 */
 
   int icm_ok = (ICM42688_Init() == ICM_OK);
@@ -558,6 +641,41 @@ void EncoderTask(void *argument)
     osDelay(10);
   }
   /* USER CODE END EncoderTask */
+}
+
+/* USER CODE BEGIN Header_MainMotorTask */
+/**
+* @brief Function implementing the MainMotorDriver thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_MainMotorTask */
+void MainMotorTask(void *argument)
+{
+  /* USER CODE BEGIN MainMotorTask */
+  /* Temporary forward/backward smoke test.
+     Replace with ROS2/Nav2 velocity command handling. */
+  const uint8_t TEST_SPEED = 150;
+  const uint32_t TEST_STEP_MS = 2000;
+
+  MotorDriver_Enable();
+
+  /* Infinite loop */
+  for(;;)
+  {
+    MotorForward_runSpeed(TEST_SPEED, TEST_SPEED);
+    osDelay(TEST_STEP_MS);
+
+    MotorForward_runSpeed(0, 0);
+    osDelay(TEST_STEP_MS);
+
+    MotorBackward_runSpeed(TEST_SPEED, TEST_SPEED);
+    osDelay(TEST_STEP_MS);
+
+    MotorForward_runSpeed(0, 0);
+    osDelay(TEST_STEP_MS);
+  }
+  /* USER CODE END MainMotorTask */
 }
 
 /**
